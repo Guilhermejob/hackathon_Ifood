8c19b599f2b8ffd4085bc1acf9cbcbc2fe2cf9a7
Guilherme Job <guilhermejob047@gmail.com>
2022-02-19 01:06:26 -0300

    feat: Create product model and made a relationship with supermarket model

---
+++ app/models/product_model.py
@@ -0,0 +1,23 @@
+from app.configs.database import db
+from dataclasses import dataclass
+from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.sql.schema import ForeignKey
+
+
+
+
+@dataclass
+class ProductModel(db.Model):
+    id:int
+    name:str
+    price:str
+    super_market_id:int
+
+    __tablename__ = 'products'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False, unique=True)
+    price = Column(Float, nullable=False)
+    super_market_id  = Column(Integer, ForeignKey(
+        'supermarkets.id'))
