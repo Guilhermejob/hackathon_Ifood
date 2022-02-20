3875ba81b179a3c11ee428d15604948777cbdc4f
Guilherme Job <guilhermejob047@gmail.com>
2022-02-20 09:39:44 -0300

    feat: route to return recipe items present in markets

--- app/routes/recipe_blueprint.py
+++ app/routes/recipe_blueprint.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from app.controllers.recipe_controller import create_recipe,get_recipe,list_recipes,get_recipes_by_name
+from app.controllers.recipe_controller import create_recipe,get_recipe,list_recipes,get_recipes_by_name,get_products_from_recipe
 
 bp = Blueprint("bp_recipe",__name__)
 
@@ -7,3 +7,4 @@ bp.post("/recipe")(create_recipe)
 bp.get("/recipe")(list_recipes)
 bp.get("/recipe/<int:id>")(get_recipe)
 bp.get("/recipe/<string:name>")(get_recipes_by_name)
+bp.get("/recipe/<int:id>/products")(get_products_from_recipe)
